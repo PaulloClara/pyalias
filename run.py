90940038fc56e1215557cae4749e929fb2421b4c
paulloclara <paulloclara@gmail.com>
2019-11-27 22:49:26 -0300

    feat: add python language

--- run.py
+++ run.py
@@ -3,29 +3,47 @@ from sys import argv
 
 
 class App:
+
     def __init__(self):
-        self.current_path = argv[1]
-        self.lang = argv[2].split('.')[-1]
-        self.file_path = '.'.join(argv[2].split('.')[:-1])
         self.args = argv[3:]
-        self.path = f'{self.current_path}/{self.file_path}.{self.lang}'
+        self.lang = argv[2].split('.')[-1]
+        self.current_path = argv[1]
+        self.file_path = '.'.join(argv[2].split('.')[:-1])
+        self.full_path = f'{self.current_path}/{self.file_path}.{self.lang}'
 
     def run(self):
         if self.lang in ['c']:
             self.run_c()
+        elif self.lang in ['py']:
+            self.run_python()
+        else:
+            print('\033[31mSorry, unsupported programming language...\033[0;0m')
 
     def run_c(self):
         output_file_path = f'{self.current_path}/run'
 
-        compile_command = ['gcc', self.path, '-o', output_file_path]
+        compile_command = ['gcc', self.full_path, '-o', output_file_path]
         run_output_file = [output_file_path]
 
         if self.args:
-            for arg in self.args:
-                run_output_file.append(arg)
+            self._add_args(run_output_file)
 
         subprocess.run(compile_command)
         subprocess.run(run_output_file)
+
+    def run_python(self):
+        run_command = ['python3', self.full_path]
+
+        if self.args:
+            self._add_args(run_command)
+
+        subprocess.run(run_command)
+
+    def _add_args(self, command):
+        for arg in self.args:
+            command.append(arg)
+
+        return command
 
 
 if __name__ == '__main__':
