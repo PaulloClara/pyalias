69f39c5ff1f384c2dad2faec1b1cfacf256ad95b
paulloclara <paulloclara@gmail.com>
2019-12-03 21:36:57 -0300

    refactor: set constant var

--- run.py
+++ run.py
@@ -1,50 +1,51 @@
-import subprocess
 from sys import argv
+from subprocess import run as run_command
+
+
+ARGS = argv[3:]
+LANG = argv[2].split('.')[-1]
+CURRENT_PATH = argv[1]
+FILE_PATH = '.'.join(argv[2].split('.')[:-1])
+FULL_PATH = f'{CURRENT_PATH}/{FILE_PATH}.{LANG}'
+TEMP_PATH = f'{"/".join(argv[0].split("/")[:-1])}/temp'
 
 
 def main():
     if len(argv) < 3:
         show_error('file not found')
 
-    args = argv[3:]
-    lang = argv[2].split('.')[-1]
-    current_path = argv[1]
-    file_path = '.'.join(argv[2].split('.')[:-1])
-    full_path = f'{current_path}/{file_path}.{lang}'
-    temp_path = f'{"/".join(argv[0].split("/")[:-1])}/temp'
-
-    if lang in ['c']:
-        run_c(temp_path=temp_path, full_path=full_path, args=args)
-    elif lang in ['py']:
-        run_python(full_path=full_path, args=args)
+    if LANG in ['c']:
+        run_c()
+    elif LANG in ['py']:
+        run_python()
     else:
         show_error(error='unsupported programming language')
 
 
-def run_c(temp_path, full_path, args):
-    output_file_path = f'{temp_path}/run'
+def run_c():
+    output_file_path = f'{TEMP_PATH}/run'
 
-    compile_command = ['gcc', full_path, '-o', output_file_path]
+    compile_command = ['gcc', FULL_PATH, '-o', output_file_path]
     execution_command = [output_file_path]
 
-    if args:
-        add_args(command=execution_command, args=args)
+    if ARGS:
+        add_args(command=execution_command, ARGS=ARGS)
 
-    subprocess.run(compile_command)
-    subprocess.run(execution_command)
-
-
-def run_python(full_path, args):
-    execution_command = ['python3', '-B', full_path]
-
-    if args:
-        add_args(command=execution_command, args=args)
-
-    subprocess.run(execution_command)
+    run_command(compile_command)
+    run_command(execution_command)
 
 
-def add_args(command, args):
-    for arg in args:
+def run_python():
+    execution_command = ['python3', '-B', FULL_PATH]
+
+    if ARGS:
+        add_args(command=execution_command, ARGS=ARGS)
+
+    run_command(execution_command)
+
+
+def add_args(command, ARGS):
+    for arg in ARGS:
         command.append(arg)
 
     return command
@@ -64,6 +65,6 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as err:
-        if type(err.args[0]) == str and '\n\n' in err.args[0]:
+        if type(err.ARGS[0]) == str and '\n\n' in err.ARGS[0]:
             raise
         show_error(error='sorry, unexpected error', stop_exec=False)
