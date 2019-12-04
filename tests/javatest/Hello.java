a9e785b305f7b00561f1894c88fb935f1193c256
paulloclara <paulloclara@gmail.com>
2019-12-03 23:03:12 -0300

    feat: add java tests

---
+++ tests/javatest/Hello.java
@@ -0,0 +1,9 @@
+package javatest;
+
+public class Hello {
+
+  public void run() {
+    System.out.print("\n\tHello World\n\n");
+  }
+
+}
---
+++ tests/javatest/Test.java
@@ -0,0 +1,11 @@
+package javatest;
+
+public class Test {
+
+  public static void main(String[] args) {
+    Hello hello = new Hello();
+
+    hello.run();
+  }
+
+}
